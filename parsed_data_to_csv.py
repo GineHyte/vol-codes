73f578e94dd95ac5fb703ce784ec22e8dc72a904
minebyte <petrlesha@gmail.com>
2024-12-15 19:44:58 +0100

    some changes

--- parsed_data_to_csv.py
+++ parsed_data_to_csv.py
@@ -7,23 +7,30 @@ from docx.table import Table
 
 import tables as tabs
 
-RESULTS = json.load(codecs.open("results.json", "r+", encoding="utf-8")) # open the file
-CODES = json.load(open("codes.json", "r+", encoding="utf-8")) # open the file
+RESULTS = json.load(
+    codecs.open("results.json", "r+", encoding="utf-8")
+)  # open the file
+CODES = json.load(open("codes.json", "r+", encoding="utf-8"))  # open the file
 DOC = Document()
 
 tab1 = tabs.Table1()
 tab2 = tabs.Table2()
 
-def tdlist_to_csv(data:list) -> str:
+
+def tdlist_to_csv(data: list) -> str:
     """Converts a list of lists to a CSV string."""
-    return '\n'.join([','.join(x) for x in data])
+    return "\n".join([",".join(x) for x in data])
+
 
 def mercells(table: Table, cells: List[Tuple[int]]) -> None:
     if len(cells) > 1:
-        try: cells[1] = table.cell(*cells[0]).merge(table.cell(*cells[1]))
-        except Exception: return cells
+        try:
+            cells[1] = table.cell(*cells[0]).merge(table.cell(*cells[1]))
+        except Exception:
+            return cells
         mercells(table, cells[1:])
     return cells
+
 
 def tdlist_to_word(doc: Document, data: list) -> Table:
     l_rows = len(data)
@@ -35,24 +42,25 @@ def tdlist_to_word(doc: Document, data: list) -> Table:
     for i, row_data in enumerate(data):
         row = table.rows[i].cells
         for j, el in enumerate(row_data):
-            if p_el == el:
-                mercells(table, [(i, j), (i, j - 1)])
+            # if p_el == el:
+            #     mercells(table, [(i, j), (i, j - 1)])
             row[j].text = el
             p_el = el
 
     # merge cells verticaly
-    for i in range(l_rows):
-        for j in range(1, l_cols):
-            try:
-                el = data[j][i]
-                if data[j-1][i] == el:
-                    mercells(table, [(j - 1, i), (j, i)])
-                row = table.rows[j].cells
-                row[i].text = el
-            except IndexError:
-                continue
+    # for i in range(l_rows):
+    #     for j in range(1, l_cols):
+    #         try:
+    #             el = data[j][i]
+    #             if data[j-1][i] == el:
+    #                 mercells(table, [(j - 1, i), (j, i)])
+    #             row = table.rows[j].cells
+    #             row[i].text = el
+    #         except IndexError:
+    #             continue
 
     return table
+
 
 if __name__ == "__main__":
     # Write the tables to CSV files
@@ -63,7 +71,10 @@ if __name__ == "__main__":
         if l in "yzwl":
             continue
         args = [x for x in data.keys() if x not in "_funczones"]
-        args = (l, args,)
+        args = (
+            l,
+            args,
+        )
 
         tdlist_to_word(DOC, tab1.table(CODES, RESULTS))
         DOC.add_page_break()
@@ -74,4 +85,3 @@ if __name__ == "__main__":
         #         f.write(tdlist_to_csv(table3.table(CODES, RESULTS, cat, *args)))
 
     DOC.save("./result.docx")
-    
--- tables.py
+++ tables.py
@@ -1,7 +1,8 @@
 from docx import Document
 
+
 class Table1:
-    def transform_data(self, results: dict)-> dict:
+    def transform_data(self, results: dict) -> dict:
         data = {}
         for category, games in results.items():
             data[category] = {}
@@ -16,28 +17,45 @@ class Table1:
                             data[value[2]][category][value[0]] = 0
 
                         data[value[2]][category][value[0]] += 1
-                        
+
                         if value[0] not in data[category]:
-                            data[category][value[0]]= 0
+                            data[category][value[0]] = 0
                         data[category][value[0]] += 1
-                        data[category]['games_cout'] = len(games)
+                        data[category]["games_cout"] = len(games)
                     except Exception as e:
                         print("Error by {} value".format(value))
         return data
 
     def table_inner(self, data: dict) -> list:
-        """generates """
+        """generates"""
         result = {}
         for category_or_tech, cat_data in data.items():
-            if len(category_or_tech) != 1: continue
+            if len(category_or_tech) != 1:
+                continue
             for loc_cat, loc_cat_data in cat_data.items():
                 try:
                     if category_or_tech not in result:
                         result[category_or_tech] = []
-                    result[category_or_tech].append(str(loc_cat_data['w']/data[loc_cat]['games_cout']).replace('.', ','))
-                    result[category_or_tech].append(f'{round(loc_cat_data["w"]*100/data[loc_cat]["w"], 2)}%'.replace('.', ','))
-                    result[category_or_tech].append(str(loc_cat_data['l']/data[loc_cat]['games_cout']).replace('.', ','))
-                    result[category_or_tech].append(f'{round(loc_cat_data["l"]*100/data[loc_cat]["l"], 2)}%'.replace('.', ','))
+                    result[category_or_tech].append(
+                        str(loc_cat_data["w"] / data[loc_cat]["games_cout"]).replace(
+                            ".", ","
+                        )
+                    )
+                    result[category_or_tech].append(
+                        f'{round(loc_cat_data["w"]*100/data[loc_cat]["w"], 2)}%'.replace(
+                            ".", ","
+                        )
+                    )
+                    result[category_or_tech].append(
+                        str(loc_cat_data["l"] / data[loc_cat]["games_cout"]).replace(
+                            ".", ","
+                        )
+                    )
+                    result[category_or_tech].append(
+                        f'{round(loc_cat_data["l"]*100/data[loc_cat]["l"], 2)}%'.replace(
+                            ".", ","
+                        )
+                    )
                 except Exception as e:
                     print("Error by {} value".format(loc_cat_data))
         return [x for x in result.values()]
@@ -45,23 +63,31 @@ class Table1:
     def table(self, codes: dict, results: dict) -> list:
         _data = self.transform_data(results)
         res = self.table_inner(_data)
-        res.insert(0, ['Технічні прийоми', *['K', '%']*6])
-        res.insert(0, ['Технічні прийоми', *[*['WIN']*2, *['LOS']*2]*3])
-        res.insert(0, ['Технічні прийоми', *['Вік 13-15 років, разів за гру']*4, 
-                    *['Вік 16-17 років, разів за гру']*4, *['Вік 18-19 років, разів за одну гру']*4])
-        for i, code in enumerate(['a', 'b', 'c', 'd', 'e', 'h']):
-            if i > len(res)-4: res.append([])
-            res[i+3].insert(0, codes[code]['_'])
+        res.insert(0, ["Технічні прийоми", *["K", "%"] * 6])
+        res.insert(0, ["Технічні прийоми", *[*["WIN"] * 2, *["LOS"] * 2] * 3])
+        res.insert(
+            0,
+            [
+                "Технічні прийоми",
+                *["Вік 13-15 років, разів за гру"] * 4,
+                *["Вік 16-17 років, разів за гру"] * 4,
+                *["Вік 18-19 років, разів за одну гру"] * 4,
+            ],
+        )
+        for i, code in enumerate(["a", "b", "c", "d", "e", "h"]):
+            if i > len(res) - 4:
+                res.append([])
+            res[i + 3].insert(0, codes[code]["_"])
 
         return res
 
 
 class Table2:
-    def transform_data(self, results: dict)-> dict:
+    def transform_data(self, results: dict) -> dict:
         data = {}
         for category, games in results.items():
             data[category] = {}
-            data[category]['games_cout'] = len(games)
+            data[category]["games_cout"] = len(games)
             for _, game_part in games.items():
                 for value in game_part:
                     try:
@@ -77,7 +103,7 @@ class Table2:
                             data[value[2]][value[3]][category][value[0]][value[1]] = 0
 
                         data[value[2]][value[3]][category][value[0]][value[1]] += 1
-                        
+
                         if value[0] not in data[category]:
                             data[category][value[0]] = {"_": 0}
                         if value[1] not in data[category][value[0]]:
@@ -103,33 +129,48 @@ class Table2:
                 for res in ["w", "l"]:
                     try:
                         if res not in cat_data[cat_age]:
-                            result[cat].extend([*['0']*2,*['0%']*2])
+                            result[cat].extend([*["0"] * 2, *["0%"] * 2])
                             continue
                         for player_type in ["y", "z"]:
                             if player_type not in cat_data[cat_age][res]:
-                                result[cat].append('0')
+                                result[cat].append("0")
                                 continue
-                            result[cat].append(f"\"{str(cat_data[cat_age][res][player_type]/data[cat_age]['games_cout']).replace('.', ',')}\"")
+                            result[cat].append(
+                                f"{str(cat_data[cat_age][res][player_type]/data[cat_age]['games_cout']).replace('.', ',')}"
+                            )
                         for player_type in ["y", "z"]:
                             if player_type not in cat_data[cat_age][res]:
-                                result[cat].append('0%')
+                                result[cat].append("0%")
                                 continue
-                            result[cat].append(f'\"{round(cat_data[cat_age][res][player_type]*100/data[cat_age][res][player_type][tech], 2)}%\"'.replace('.', ','))
+                            result[cat].append(
+                                f"{round(cat_data[cat_age][res][player_type]*100/data[cat_age][res][player_type][tech], 2)}%".replace(
+                                    ".", ","
+                                )
+                            )
                     except Exception as e:
                         print(cat_data)
         return result
 
-    def table(self, codes: dict, results: dict, code_tech: str, codes_to_show: list) -> list:
+    def table(
+        self, codes: dict, results: dict, code_tech: str, codes_to_show: list
+    ) -> list:
         data = self.transform_data(results)
         res_inner = self.table_inner(data, code_tech)
-        res=[['Технічні прийоми', *['Вік 13-15 років, разів за гру']*8, 
-            *['Вік 16-17 років, разів за гру']*8, *['Вік 18-19 років, разів за одну гру']*8],
-            ['Технічні прийоми', *[*['WIN']*4, *['LOS']*4]*3],
-            ['Технічні прийоми', *[*['K']*2, *['%']*2]*6],
-            ['Технічні прийоми', *['Б', 'З']*12]]
+        res = [
+            [
+                "Технічні прийоми",
+                *["Вік 13-15 років, разів за гру"] * 8,
+                *["Вік 16-17 років, разів за гру"] * 8,
+                *["Вік 18-19 років, разів за одну гру"] * 8,
+            ],
+            ["Технічні прийоми", *[*["WIN"] * 4, *["LOS"] * 4] * 3],
+            ["Технічні прийоми", *[*["K"] * 2, *["%"] * 2] * 6],
+            ["Технічні прийоми", *["Б", "З"] * 12],
+        ]
 
         for i, code in enumerate(codes_to_show):
-            if i > len(res)-5: res.append([])
-            res[i+4].insert(0, codes[code_tech][code])
-            res[i+4].extend(res_inner[code] if code in res_inner else ['0']*24)
+            if i > len(res) - 5:
+                res.append([])
+            res[i + 4].insert(0, codes[code_tech][code])
+            res[i + 4].extend(res_inner[code] if code in res_inner else ["0"] * 24)
         return res
